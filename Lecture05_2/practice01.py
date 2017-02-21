92c714c5e46086292a8991bc62c99be9ca7ec23e
Brian Chun <brianchun@I-Am-Ur-Father.local>
2017-02-21 21:00:33 +0900

    구구단(19단) 사용자 입력에 의해 처리되게 만들어봄

--- Lecture05_2/practice01.py
+++ Lecture05_2/practice01.py
@@ -1,9 +1,13 @@
-x = int(input('Enter an integer ranging from 2-19: '))
-y = 1
-if 1 < x < 20:
-	while y < 20:
-		print(x, "*" , y , " =" , x * y)
-		y = y + 1
+def multitable_19():
 
-else: 
-	print('That number is not valid.')
+	x = int(input('Enter an integer ranging from 2-19: '))
+	y = 1
+	if 1 < x < 20:
+		while y < 20:
+			print(x, "*" , y , " =" , x * y)
+			y = y + 1
+
+	else: 
+		print('That number is not valid.')
+
+multitable_19()
